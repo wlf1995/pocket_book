269dc682083a79e809f6f7dbdd27ccc9cb8a27b5
wanglifang <1181649564@qq.com>
2019-08-02 14:30:17 +0800

    修改

--- src/main/java/com/ibicn/hr/entity/base/BaseEntity.java
+++ src/main/java/com/ibicn/hr/entity/base/BaseEntity.java
@@ -2,8 +2,6 @@ package com.ibicn.hr.entity.base;
 
 import com.ibicn.hr.ENUM.EnumBaseStatus;
 import com.ibicn.hr.ENUM.EnumUtil;
-import com.ibicn.hr.ENUM.EnumXueli;
-import com.ibicn.hr.entity.sys.SystemUser;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
@@ -32,24 +30,24 @@ public abstract class BaseEntity {
      * 创建时间
      **/
     @CreatedDate
-    @Column(name = "createdtime",nullable = false)
+    @Column(name = "createdtime", nullable = false)
     private Date createdTime;
     /**
      * 修改时间
      **/
     @LastModifiedDate
-    @Column(name = "updatedtime",nullable = false)
+    @Column(name = "updatedtime", nullable = false)
     private Date updatedTime;
     /**
      * 修改次数
      **/
-    @Column(name = "updatedcount",nullable = false)
-    private Date updatedCount;
+    @Column(name = "updatedcount", nullable = false)
+    private Integer updatedCount;
     /**
      * 状态 0为停用状态，1为正常状态,默认为正常状态
      **/
-    @Column(name = "status",nullable = false)
-    private EnumBaseStatus status=EnumBaseStatus.正常;
+    @Column(name = "status", nullable = false)
+    private EnumBaseStatus status = EnumBaseStatus.正常;
 
     /**
      * 添加人
