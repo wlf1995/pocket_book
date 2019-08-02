2cb9770b74415452301738763d858058a6282f70
wanglifang <1181649564@qq.com>
2019-08-02 14:21:02 +0800

    修改

--- src/main/java/com/ibicn/hr/config/UserAuditorAware.java
+++ src/main/java/com/ibicn/hr/config/UserAuditorAware.java
@@ -3,10 +3,9 @@ package com.ibicn.hr.config;
 import com.ibicn.hr.entity.sys.SystemUser;
 import com.ibicn.hr.service.sys.SystemUserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -17,7 +16,7 @@ import java.util.Optional;
  * @CreatedBy
  * @LastModifiedBy 自动注入用户
  */
-@Configuration
+@Component
 public class UserAuditorAware implements AuditorAware<Integer> {
     @Autowired
     SystemUserServiceI userServiceI;
@@ -30,9 +29,4 @@ public class UserAuditorAware implements AuditorAware<Integer> {
         //TODO: 根据实际情况取真实用户
         return Optional.of(curUser.getId());
     }
-
-//    @Bean
-//    public UserAuditorAware setUserAuditorAware() {
-//        return new UserAuditorAware();
-//    }
 }
